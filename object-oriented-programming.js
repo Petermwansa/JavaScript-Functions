4b1b092a320a3a63cdb2ef79cda03d8016708baa
Peter Mwansa <mwansapeter800@gmail.com>
2022-08-08 22:45:50 +0300

    More on OOP

--- object-oriented-programming.js
+++ object-oriented-programming.js
@@ -146,3 +146,171 @@ function joinDogFraternity(candidate) {
 }
   
 console.log(joinDogFraternity(Dog));
+
+
+
+
+
+
+//PROTOTYPE TO A NEW OBJECT
+function Dog(name) {
+    this.name = name;
+}
+  
+Dog.prototype = {
+    numLegs: 4,
+    eat: function () {
+  
+    },
+    describe: function() {
+      
+    }
+  
+};
+
+
+
+
+
+//SET THE CONSTRUCTOR PROPERTY WHEN CHANGING THE PROTOTYPE
+function Dog(name) {
+    this.name = name;
+}
+  
+Dog.prototype = {
+    //Whenever the prototype is manually set to a new object, remember to define the constructor property
+    constructor: Dog,
+    numLegs: 4,
+    eat: function() {
+      console.log("nom nom nom");
+    },
+    describe: function() {
+      console.log("My name is " + this.name);
+    }
+};
+
+
+
+
+//WHERE OBJECT'S PROTOTYPE COMES FROM
+function Dog(name) {
+    this.name = name;
+  }
+  function Bird(name) {
+    this.name - name;
+  }
+  
+  let beagle1 = new Dog("Snoopy");
+  let eagle = new Bird("The Eyer");
+
+  console.log(Bird.prototype.isPrototypeOf(eagle));
+  console.log(Dog.prototype.isPrototypeOf(beagle1));
+
+
+
+
+//THE PROTOTYPE CHAIN
+function Dog(name) {
+    this.name = name;
+}
+  
+let beagle2 = new Dog("Snoopy");
+  
+Dog.prototype.isPrototypeOf(beagle2);  // yields true
+  
+//this is the prototype chain. 'Object' is the supertype for all objects in JS
+Object.prototype.isPrototypeOf(Dog.prototype);
+
+
+
+
+
+
+//USING INHERITANCE TO AVOID REPEATING YOURSELF
+function Cat(name) {
+    this.name = name;
+  }
+  
+  Cat.prototype = {
+    constructor: Cat
+  };
+  
+  function Bear(name) {
+    this.name = name;
+  }
+  
+  Bear.prototype = {
+    constructor: Bear
+  };
+  
+  function Animal() { }
+  
+  //to keep the DRY principle, we put the eat() in the Animal supertype
+  Animal.prototype = {
+    constructor: Animal,
+    eat: function () {
+      console.log("nom nom nom")
+    }
+  };
+
+
+
+
+
+//INHERIT THE BEHAVIORS FROM A SUPERTYPE
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+let duck3 = Object.create(Animal.prototype);
+let beagle3 = Object.create(Animal.prototype);  //Object.create(obj) creates a new object, and sets obj as the new object's prototype
+
+
+
+
+
+
+//Set the Child's Prototype to an Instance of the Parent
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+Dog.prototype = Object.create(Animal.prototype);
+
+let beagle4 = new Dog();
+
+console.log(beagle4);
+
+
+
+
+
+//RESET THE INHERITED CONSTRUCTOR PROPERTIES
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Only change code below this line
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+
+let duck5 = new Bird();
+duck.constructor;
+let beagle5 = new Dog();
+beagle.constructor;
